ac0ff7b60d58796b71563679c0eb3cfbb5e3ee86
magnanimous-se7en <jl.parrales.torres@gmail.com>
2024-11-07 15:14:04 -0500

    Actualizacion useupload

--- desarrollo/the-vault/proyecto/the-vault/hooks/useUpload.ts
+++ desarrollo/the-vault/proyecto/the-vault/hooks/useUpload.ts
@@ -6,12 +6,6 @@ interface UploadStore {
     onClose: () => void
 }
 
-interface UploadStore {
-    isOpen: boolean
-    onOpen: () => void
-    onClose: () => void
-}
-
 const useUpload = create<UploadStore>((set) => ({
     isOpen: false,
     onOpen: () => set({ isOpen: true }),
