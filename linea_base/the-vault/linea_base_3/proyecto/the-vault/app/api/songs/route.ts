dbf675b46d5e681e73a362270d98a3956856f9b4
sofiaaaa <145217327+saaaan25@users.noreply.github.com>
2024-11-28 13:00:58 -0500

    Delete linea_base directory

--- linea_base/the-vault/linea_base_3/proyecto/the-vault/app/api/songs/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from 'next/server'
-import getSongs from '@/actions/getSongs'
-
-export async function GET() {
-  const songs = await getSongs()
-  return NextResponse.json(songs)
-}
